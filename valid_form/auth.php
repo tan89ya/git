b0bc81829ff55d3801ee160c85d0720323f367a9
tan89ya <tan89ya@mail.ru>
2024-11-27 10:22:57 +0300

    first commit

---
+++ valid_form/auth.php
@@ -0,0 +1,23 @@
+<?php
+	$login = filter_var(trim($_POST["login"]),
+	FILTER_SANITIZE_STRING);
+	$pass = filter_var(trim($_POST["pass"]),
+	FILTER_SANITIZE_STRING);
+
+	$pass = md5($pass."mnljk24l1kj2hnk3");
+
+	$mysql = new mysqli('mariadb-10.3','root','','register-bg');
+
+	$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'");
+	$user = $result->fetch_assoc();
+	if(count($user) == 0) {
+		echo "Такой пользователь не найден";
+		exit();
+	}
+
+	setcookie("user", $user["name"], time() + 3600,"/");
+
+
+	$mysql->close();
+
+	header("Location: /");
